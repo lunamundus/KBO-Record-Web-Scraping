9f41041831c5422a2f43ab6962d822a3bf346642
luna <lunamundus1007@gmail.com>
2022-12-13 22:42:14 +0900

    Combine the code to one python file

--- kbo.py
+++ kbo.py
@@ -10,7 +10,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome("chrome_driver\chromedriver.exe", options=options)
 
-# 홈페이지로 이동
+# 메인 홈페이지로 이동
 driver.get("https://www.koreabaseball.com/")
 
 # 기록 페이지로 이동
@@ -23,44 +23,84 @@ css_selector = "#lnb02"
 elem = driver.find_element(By.CSS_SELECTOR, css_selector)
 elem.click()
 
-# 팀 선택
-select = Select(driver.find_element(By.NAME, "ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$ddlTeam$ddlTeam"))
-select.select_by_value(value='OB')
+# 기록실 옵션(타자, 투수, 수비, 주루)
+options = ['hitter', 'pitcher', 'defense', 'runner']
+record_tab_xpath = '//*[@id="contents"]/div[2]/div[2]/ul/*'
 
-time.sleep(3)
+for i in range(0, 4):
+    # 현재 기록실 옵션 상태
+    status = options[i]
+    
+    # 원하는 기록실 옵션 클릭
+    record_tab = driver.find_elements(By.XPATH, record_tab_xpath)
+    record_tab[i].click()
+    
+    time.sleep(3)
 
-# 데이터 가져오기
-thead_xpath = '//*[@id="cphContents_cphContents_cphContents_udpContent"]/div[3]/table/thead/tr'
-tbody_xapth = '//*[@id="cphContents_cphContents_cphContents_udpContent"]/div[3]/table/tbody'
+    # 팀 선택
+    select = Select(driver.find_element(By.NAME, "ctl00$ctl00$ctl00$cphContents$cphContents$cphContents$ddlTeam$ddlTeam"))
+    select.select_by_value(value='OB')
 
-table_head = driver.find_element(By.XPATH, thead_xpath).text.split(' ')
-table_value = driver.find_element(By.XPATH, tbody_xapth).text.split('\n')
+    time.sleep(3)
 
-# 2페이지 이동
-id_selector = 'cphContents_cphContents_cphContents_ucPager_btnNo2'
-elem = driver.find_element(By.ID, id_selector)
-elem.click()
+    # 데이터 가져오기 & 가공
+    table_value = driver.find_element(By.CLASS_NAME, 'tData01').text.split('\n')
+    table_head = table_value.pop(0).split(' ')
 
-time.sleep(3)
+    ## 페이지 이동 및 데이터 가져오기 & 가공
+    pages = driver.find_element(By.CLASS_NAME, 'paging').text.split(' ')
+    if len(pages) == 1:
+        pass
+    else:
+        for page in range(1, len(pages)):
+            id_selector = 'cphContents_cphContents_cphContents_ucPager_btnNo' + str(page+1)
+            elem = driver.find_element(By.ID, id_selector)
+            elem.click()
 
-# 2페이지 데이터 추가
-temp = driver.find_element(By.XPATH, tbody_xapth).text.split('\n')
-table_value.extend(temp)
+            time.sleep(3)
 
-# 데이터 리스트로 변환
-record_values = []
+            temp = driver.find_element(By.CLASS_NAME, 'tData01').text.split('\n')
+            del temp[0]
+            table_value.extend(temp)
 
-for item in table_value:
-    record_values.append(item.split(' '))
+    # 선수 기록 데이터 가공
+    record_values = []
 
-# 데이터프레임 생성
-doosan_dataframe = pd.DataFrame(columns=table_head)
+    for item in table_value:
+        record_values.append(item.split(' '))
+    
+    if status == 'pitcher':
+        for item in record_values:
+            if len(item) == len(table_head):
+                pass
+            else:
+                temp1 = item.pop(10)
+                temp2 = item.pop(10)
+                res_temp = temp1 + ' ' + temp2
+                item.insert(10, res_temp)
 
-for i in range(0, len(record_values)):
-    doosan_dataframe.loc[i] = record_values[i]
+    if status == 'defense':
+        for item in record_values:
+            if len(item) == len(table_head):
+                pass
+            else:
+                temp1 = item.pop(6)
+                temp2 = item.pop(6)
+                res_temp = temp1 + ' ' + temp2
+                item.insert(6, res_temp)
+    
+    # 데이터 -> 데이터 프레임 형태로 변환
+    globals()[f'{status}_dataframe'] = pd.DataFrame(columns=table_head)
+    
+    for j in range(0, len(record_values)):
+        globals()[f'{status}_dataframe'].loc[j] = record_values[j]
+
+    time.sleep(2)
 
 # 엑셀 파일로 저장
-doosan_dataframe.to_excel('test.xlsx', index=False)
+with pd.ExcelWriter('test.xlsx') as writer:
+    for status in options:
+        globals()[f'{status}_dataframe'].to_excel(writer, sheet_name=status, index=False)
 
 # 창 닫기
 driver.close()
