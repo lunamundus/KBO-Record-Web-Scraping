4b040728c69f73929d2e810f50154203c7bab6ff
Oh Byeongkuk <lunamundus1007@gmail.com>
2022-12-31 16:38:12 +0900

    ver. 1.2.0

--- kbo_record_scraping.py
+++ kbo_record_scraping.py
@@ -44,9 +44,19 @@ for i in range(0, 4):
     record_tab[i].click()
     
     time.sleep(3)
-
+    
     table_head = driver.find_element(By.CLASS_NAME, 'tData01').text.split('\n').pop(0).split(' ')
+
+    if status == 'hitter' or status == 'pitcher':
+        driver.find_element(By.CLASS_NAME, 'next').click()
+        table_head_temp = driver.find_element(By.CLASS_NAME, 'tData01').text.split('\n').pop(0).split(' ')
+        for i in range(0, 4):
+            del table_head_temp[0]
+        table_head = table_head + table_head_temp
+        driver.find_element(By.CLASS_NAME, 'prev').click()
+    
     table_value = []
+    table_value2 = []
 
     # 팀 선택
     for team in teams:
@@ -79,12 +89,55 @@ for i in range(0, 4):
                 del temp[0]
                 table_value.extend(temp)
 
+        if status == 'hitter' or status == 'pitcher':
+            driver.find_element(By.CLASS_NAME, 'next').click()
+            pages = driver.find_element(By.CLASS_NAME, 'paging').text.split(' ')
+
+            if len(pages) == 1:
+                id_selector = 'cphContents_cphContents_cphContents_ucPager_btnNo' + str(pages[0])
+                elem = driver.find_element(By.ID, id_selector)
+                elem.click()
+
+                time.sleep(3)
+
+                temp = driver.find_element(By.CLASS_NAME, 'tData01').text.split('\n')
+                del temp[0]
+                table_value2.extend(temp)
+            else:
+                for page in range(1, len(pages)+1):
+                    id_selector = 'cphContents_cphContents_cphContents_ucPager_btnNo' + str(page)
+                    elem = driver.find_element(By.ID, id_selector)
+                    elem.click()
+
+                    time.sleep(3)
+
+                    temp = driver.find_element(By.CLASS_NAME, 'tData01').text.split('\n')
+                    del temp[0]
+                    table_value2.extend(temp)
+            
+            driver.find_element(By.CLASS_NAME, 'prev').click()
+
     # 선수 기록 데이터 가공
     record_values = []
 
-    for item in table_value:
-        record_values.append(item.split(' '))
-    
+    if status == 'hitter' or status == 'pitcher':
+        temp_values = []
+        temp_values2 = []
+
+        for item, item2 in zip(table_value, table_value2):
+            temp_values.append(item.split(' '))
+            temp_values2.append(item2.split(' '))
+        
+        for i in range(0, len(temp_values2)):
+            for j in range(0, 4):
+                del temp_values2[i][0]
+        
+        for item, item2 in zip(temp_values, temp_values2):
+            record_values.append(item + item2)
+    else:
+        for item in table_value:
+            record_values.append(item.split(' '))
+
     if status == 'pitcher':
         for item in record_values:
             if len(item) == len(table_head):
@@ -114,9 +167,15 @@ for i in range(0, 4):
     time.sleep(2)
 
 # 엑셀 파일로 저장
-with pd.ExcelWriter('result/kbo_players_record.xlsx') as writer:
+with pd.ExcelWriter('result/kbo_players_record_extension.xlsx') as writer:
     for status in options:
         globals()[f'{status}_dataframe'].to_excel(writer, sheet_name=status, index=False)
+
+
+# 엑셀 파일로 저장 (테스트용)
+# with pd.ExcelWriter('test_result/test4.xlsx') as writer:
+#     for status in options:
+#         globals()[f'{status}_dataframe'].to_excel(writer, sheet_name=status, index=False)
 
 # 창 닫기
 driver.close()
